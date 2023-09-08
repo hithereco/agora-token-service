a22bbd6a56da2b0ccfbf2f4ec7a2a10609c314d1
jaehue <bbugguj@gmail.com>
2023-09-08 14:18:38 +0900

    setup lambda

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/AgoraIO-Community/agora-token-service/service"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
+
+var ginLambda *ginadapter.GinLambda
 
 func main() {
 	s := service.NewService()
-	// Stop is called on another thread, but waits for an interrupt
-	go s.Stop()
-	s.Start()
+
+	env := os.Getenv("GIN_MODE")
+	if env == "release" {
+		ginEngine, ok := s.Server.Handler.(*gin.Engine)
+		if ok {
+			ginLambda = ginadapter.New(ginEngine)
+			lambda.Start(Handler)
+		}
+	} else {
+		// Stop is called on another thread, but waits for an interrupt
+		go s.Stop()
+		s.Start()
+	}
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, request)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/AgoraIO-Community/go-tokenbuilder v1.3.0
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.14.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.3.0
 )
